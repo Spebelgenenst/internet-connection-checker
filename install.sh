078a487e81cb2a87abdc1fb62ab82a070b9ac1cf
Spebell <sebastian@die-sperlinge.de>
2026-01-24 13:38:11 +0100

    just some tests

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ git clone https://github.com/Spebelgenenst/internet-connection-checker
 cd internet-connection-checker/
 chmod +x internet-connection-checker.desktop
 mv internet-connection-checker.desktop ~/.config/autostart/
-echo now u can konfigure sis! jst run "nano config.json" uwu
+#nano internet-connection-checker/config.json
+nano config.json
