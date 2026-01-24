58f676c6f7d9a07fd6aa337690d324b0d8e70d74
Spebell <sebastian@die-sperlinge.de>
2026-01-24 13:41:24 +0100

    reversed tests or somthing

--- install.sh
+++ install.sh
@@ -3,5 +3,4 @@ git clone https://github.com/Spebelgenenst/internet-connection-checker
 cd internet-connection-checker/
 chmod +x internet-connection-checker.desktop
 mv internet-connection-checker.desktop ~/.config/autostart/
-#nano internet-connection-checker/config.json
-nano config.json
+echo to configure run "nano ~/internet-connection-checker/config.json" uwu
