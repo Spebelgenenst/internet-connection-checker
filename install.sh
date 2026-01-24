0944d3d985aba9564d9a63165e200117fe595aef
Spebell <sebastian@die-sperlinge.de>
2026-01-24 13:34:53 +0100

    added content to deskop file, made some changes in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 cd
 git clone https://github.com/Spebelgenenst/internet-connection-checker
-mv internet-connection-checker/internet-connection-checker.desktop .config/autostart/
 cd internet-connection-checker/
+chmod +x internet-connection-checker.desktop
+mv internet-connection-checker.desktop ~/.config/autostart/
 echo now u can konfigure sis! jst run "nano config.json" uwu
