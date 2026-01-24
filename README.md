e29507de54ba5fb7ff03acc96b5e4cbd5dd72f55
Spebell <sebastian@die-sperlinge.de>
2026-01-24 13:26:56 +0100

    added install.sh, missing content inter...desktop and link to somth in se readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# start-on-wifi
+# internet connection checker
 A python script which runs a programm, as soon as the device has an internet connection
+
+## installation
+
+```
+curl -sSL https://example.com/skript.sh | bash
+```
---
+++ install.sh
@@ -0,0 +1,6 @@
+cd
+pip3 install requests
+git clone https://github.com/Spebelgenenst/internet-connection-checker
+mv internet-connection-checker/internet-connection-checker.desktop .config/autostart/
+cd internet-connection-checker/
+echo now u can konfigure sis! jst run "nano config.json" uwu
